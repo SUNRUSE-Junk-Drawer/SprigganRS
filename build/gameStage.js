7165fea82794dea6e2a27090cf32344f6b212461
jameswilddev <jameswilddev@outlook.com>
2018-09-28 22:03:51 +0100

    Implemented a basic bootloader.

--- build/gameStage.js
+++ build/gameStage.js
@@ -51,7 +51,8 @@ export default class GameStage extends WatchableStage {
           <meta name="viewport" content="initial-scale=1, minimum-scale=1, maximum-scale=1, width=device-width, height=device-height, user-scalable=no">
           ${favicons.response.html.join(``)}
         </head>
-        <body style="background: black">
+        <body style="background: black; color: white;">
+          <div id="message" style="position: fixed; font-family: sans-serif; font-size: 0.5cm; top: 50%; line-height: 0.5cm; transform: translateY(-50%); left: 0; right: 0; text-align: center;">Loading bootloader; please ensure that JavaScript is enabled.</div>
           <script src="bootloader.js"></script>
         </body>
       </html>`
