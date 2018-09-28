985e79a2125ddc7030998826c148e66c5628be66
jameswilddev <jameswilddev@outlook.com>
2018-09-28 19:54:25 +0100

    The "dist" directory is now recreated at startup.

--- build/rootStage.js
+++ build/rootStage.js
@@ -3,6 +3,8 @@ import FileListStage from "./fileListStage"
 import GameStage from "./gameStage"
 import WatchableStage from "./watchableStage"
 import JavaScriptParseStage from "./javaScriptParseStage"
+import DeleteDirectoryStage from "./deleteDirectoryStage"
+import CreateDirectoryStage from "./createDirectoryStage"
 
 export default class RootStage extends WatchableStage {
   constructor(parent, name, dependencies, isOneOff) {
@@ -12,10 +14,13 @@ export default class RootStage extends WatchableStage {
     const engine = new JavaScriptParseStage(this, `engine`, [], true, () => [`src`, `engine`])
     this.watchInstanced(() => [`src`, `engine`], engine, `read`, null)
 
+    const deleteDistDirectory = new DeleteDirectoryStage(this, `deleteDistDirectory`, [], () => [`dist`])
+    const createDistDirectory = new CreateDirectoryStage(this, `createDistDirectory`, [deleteDistDirectory], () => [`dist`])
+
     const games = new FileListStage(
       this,
       `games`,
-      [],
+      [createDistDirectory],
       true,
       instance => new GameStage(games, instance, [], engine),
       () => [`src`, `games`]
