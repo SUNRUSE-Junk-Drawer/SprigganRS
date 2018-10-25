61f8c3b021ae73b024064e0d199e8273773ec01b
jameswilddev <jameswilddev@outlook.com>
2018-10-25 07:52:12 +0100

    Directory separators are now normalized to forward slashes.

--- build/run.js
+++ build/run.js
@@ -1,7 +1,20 @@
 export default (paths, onSuccess, onDone) => {
+  normalizePaths(paths)
+
   console.log(`Running for files:`)
   Object
     .keys(paths)
     .forEach(key => console.log(`\t"${key}" @ ${paths[key]}`))
+
   onSuccess()
+}
+
+function normalizePaths(paths) {
+  Object
+    .keys(paths)
+    .forEach(path => {
+      const modified = paths[path]
+      delete paths[path]
+      paths[path.replace(/\\/g, `/`)] = modified
+    })
 }
