aa86ca0d6b8f0922b032b0fc40db39e988cdd647
jameswilddev <jameswilddev@outlook.com>
2018-10-26 07:32:01 +0100

    Added a local HTTP server in watch mode.

--- build/watch.js
+++ build/watch.js
@@ -1,60 +1,80 @@
+import * as path from "path"
 import * as chokidar from "chokidar"
+import mkdirp from "mkdirp"
+import newExpress from "express"
+import * as express from "express"
 import run from "./run"
 
-let running = false
-let invalidated = false
-let throttling = null
-const paths = {}
-
-chokidar
-  .watch(`src`)
-  .on(`add`, (path, stats) => handle(`add`, path, stats))
-  .on(`change`, (path, stats) => handle(`change`, path, stats))
-  .on(`unlink`, path => {
-    console.log(`"unlink" of "${path}"`)
-    delete paths[path]
-    invalidate()
-  })
-  .on(`error`, error => { throw error })
-
-function handle(event, path, stats) {
-  console.log(`"${event}" of "${path}"`)
-  if (!stats) {
-    throw `No stats for "${event}" of "${path}"`
-  }
-  paths[path] = stats.mtime.getTime()
-  invalidate()
-}
-
-function invalidate() {
-  if (running) {
-    console.log(`Waiting to restart...`)
-    invalidated = true
-    return
+const distPath = path.join(`dist`, `watch`)
+console.log(`Creating "${distPath}" if it does not exist...`)
+mkdirp(distPath, error => {
+  if (error) {
+    throw error
   }
 
-  if (throttling == null) {
-    console.log(`Throttling...`)
-  } else {
-    console.log(`Continuing to throttle...`)
-    clearTimeout(throttling)
-  }
+  console.log(`Starting web server on port 5000...`)
 
-  throttling = setTimeout(() => {
-    console.log(`Starting...`)
-    throttling = null
-    invalidated = false
-    run(
-      JSON.parse(JSON.stringify(paths)),
-      `watch`,
-      error => console.error(`Failed; "${error}".`),
-      () => {
-        console.log(`Done.`)
-        running = false
-        if (invalidated) {
+  newExpress()
+    .use(express.static(distPath))
+    .listen(5000, () => {
+      console.log(`Watching for files...`)
+
+      let running = false
+      let invalidated = false
+      let throttling = null
+      const paths = {}
+
+      chokidar
+        .watch(`src`)
+        .on(`add`, (path, stats) => handle(`add`, path, stats))
+        .on(`change`, (path, stats) => handle(`change`, path, stats))
+        .on(`unlink`, path => {
+          console.log(`"unlink" of "${path}"`)
+          delete paths[path]
           invalidate()
+        })
+        .on(`error`, error => { throw error })
+
+      function handle(event, path, stats) {
+        console.log(`"${event}" of "${path}"`)
+        if (!stats) {
+          throw `No stats for "${event}" of "${path}"`
         }
+        paths[path] = stats.mtime.getTime()
+        invalidate()
       }
-    )
-  }, 200)
-}
+
+      function invalidate() {
+        if (running) {
+          console.log(`Waiting to restart...`)
+          invalidated = true
+          return
+        }
+
+        if (throttling == null) {
+          console.log(`Throttling...`)
+        } else {
+          console.log(`Continuing to throttle...`)
+          clearTimeout(throttling)
+        }
+
+        throttling = setTimeout(() => {
+          console.log(`Starting...`)
+          throttling = null
+          invalidated = false
+          run(
+            JSON.parse(JSON.stringify(paths)),
+            `watch`,
+            error => console.error(`Failed; "${error}".`),
+            () => {
+              console.log(`Done.`)
+              running = false
+              if (invalidated) {
+                invalidate()
+              }
+            }
+          )
+        }, 200)
+      }
+    })
+})
